f6e82e44f472b6b520c7ea7a0dde72def82b7b36
vic <vicxizhu@gmail.com>
2015-09-15 17:35:37 -0400

    implement crud, add some styling

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,20 +2,19 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-
 var refresh = function() {
-  $http.get('/contactlist').success(function(response) {
+  $http.get('/todolist').success(function(response) {
     console.log("I got the data I requested");
-    $scope.contactlist = response;
-    $scope.contact = "";
+    $scope.todolist = response;
+    $scope.todo = "";
   });
 };
 
 refresh();
 
-$scope.addContact = function() {
-  console.log($scope.contact);
-  $http.post('/contactlist', $scope.contact).success(function(response) {
+$scope.addTodo = function() {
+  console.log($scope.todo);
+  $http.post('/todolist', $scope.todo).success(function(response) {
     console.log(response);
     refresh();
   });
@@ -23,27 +22,27 @@ $scope.addContact = function() {
 
 $scope.remove = function(id) {
   console.log(id);
-  $http.delete('/contactlist/' + id).success(function(response) {
+  $http.delete('/todolist/' + id).success(function(response) {
     refresh();
   });
 };
 
 $scope.edit = function(id) {
   console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
-    $scope.contact = response;
+  $http.get('/todolist/' + id).success(function(response) {
+    $scope.todo = response;
   });
-};  
+};
 
 $scope.update = function() {
-  console.log($scope.contact._id);
-  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+  console.log($scope.todo._id);
+  $http.put('/todolist/' + $scope.todo._id, $scope.todo).success(function(response) {
     refresh();
   })
 };
 
 $scope.deselect = function() {
-  $scope.contact = "";
+  $scope.todo = "";
 }
 
 }]);﻿
--- server.js
+++ server.js
@@ -3,50 +3,50 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+var db = mongojs('todolist', ['todolist']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/contactlist', function (req, res) {
+app.get('/todolist', function (req, res) {
   console.log('I received a GET request');
 
-  db.contactlist.find(function (err, docs) {
+  db.todolist.find(function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
 });
 
-app.post('/contactlist', function (req, res) {
+app.post('/todolist', function (req, res) {
   console.log(req.body);
-  db.contactlist.insert(req.body, function(err, doc) {
+  db.todolist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
 });
 
-app.delete('/contactlist/:id', function (req, res) {
+app.delete('/todolist/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.todolist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
-app.get('/contactlist/:id', function (req, res) {
+app.get('/todolist/:id', function (req, res) {
   var id = req.params.id;
   console.log(id);
-  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+  db.todolist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
 });
 
-app.put('/contactlist/:id', function (req, res) {
+app.put('/todolist/:id', function (req, res) {
   var id = req.params.id;
   console.log(req.body.name);
-  db.contactlist.findAndModify({
+  db.todolist.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    update: {$set: {text: req.body.text}},
     new: true}, function (err, doc) {
       res.json(doc);
     }
